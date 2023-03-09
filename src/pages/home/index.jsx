6dccca41a736faafcd26b5a3076df350498a5b26
Abhishek <abhikharpal@gmail.com>
2023-03-09 22:08:04 +0530

    feat: add sidebar

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,8 +2,30 @@ import './style.css';
 
 const Home = () => {
   return (
-    <div className='login-page'>
-      HOME_PAGE
+    <div className='home-container'>
+      <div className='sidebar'>
+        <div className='title basic-padding'>
+          <p>CMS+</p>
+        </div>
+        <div className='collection-holder basic-padding'>
+          <p>COLLECTION TYPES</p>
+          <ul>
+            <li>Collection 1</li>
+            <li>Collection 2</li>
+            <li>Collection 3</li>
+            <li>Collection 4</li>
+            <li>Collection 5</li>
+            {/* if contains more make +n val */}
+          </ul>
+        </div>
+        <div className='builder-holder basic-padding'>
+          <p>CONTENT TYPE BUILDER</p>
+        </div>
+      </div>
+
+      <div className='main-container'>
+
+      </div>
     </div>
   );
 };
